cd3edf3ef99680c4340fb07ff7944dff8d09bf22
Nikolay Nemshilov <nemshilov@gmail.com>
2017-01-15 13:24:46 +1100

    Adding default fallback and favicon sources

--- src/config.js
+++ src/config.js
@@ -1,13 +1,18 @@
-module.exports = function(options) {
+const fs = require('fs');
+
+const defaultFavicon = `${__dirname}/../test/site/favicon.ico`;
+const defaultFallack = `${__dirname}/../test/site/404.html`;
+
+module.exports = function(options = {}) {
   const root = options.root || process.cwd();
   const favicon = `${root}/favicon.ico`;
   const fallback = `${root}/404.html`;
 
   return Object.assign({
     root,
-    favicon,
-    fallback,
-    port: process.env.PORT || 8080,
-    gzip: true
+    favicon:  fs.existsSync(favicon) ? favicon : defaultFavicon,
+    fallback: fs.existsSync(fallback) ? fallback : defaultFallack,
+    port:     process.env.PORT || 8080,
+    gzip:     true
   }, options);
 };

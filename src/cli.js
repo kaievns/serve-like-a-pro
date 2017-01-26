f3afb8a7d9637c590587b158af866c7ad8c110e4
Nikolay Nemshilov <nemshilov@gmail.com>
2017-01-27 09:55:40 +1100

    Adding some basic CLI options

--- src/cli.js
+++ src/cli.js
@@ -2,13 +2,42 @@
 
 const serve = require('./index');
 const args  = process.argv.slice(2);
+const opts  = findOptions(args);
 const root  = args[0];
 
-if (root === '-h' || root === '--help') {
+if (opts.help) {
   console.log(`
   USAGE:
-    serve-like-a-pro [dirname]
+    serve-like-a-pro [dirname] [OPTIONS]
+
+  OPTIONS:
+    -f --fallback file
+    -v --verbose
+    -h --help
   `.trim());
 } else {
-  serve({ root });
+  serve(Object.assign({ root }, opts));
+}
+
+function findOptions(args) {
+  return args.reduce((options, arg, i) => {
+    switch (arg) {
+      case '-h':
+      case '--help':
+        options.help = true;
+        break;
+
+      case '-f':
+      case '--fallback':
+        options.fallback = args[i+1];
+        break;
+
+      case '-v':
+      case '--verbose':
+        options.log = true;
+        break;
+    }
+
+    return options;
+  }, {});
 }
